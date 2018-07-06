bd59d76a4a478764c2d9e97703e1bfbf8f04f099
eventia <eventia@gmail.com>
2018-07-06 13:32:10 +0900

    SpringLecture 2018-07-06

--- s03/src/main/java/s02/MainClass.java
+++ s03/src/main/java/s02/MainClass.java
@@ -6,8 +6,8 @@ public class MainClass {
 		MyCalucator myCalucator = new MyCalucator();
 		myCalucator.setCalculator(new Calculator());
 
-		myCalucator.setFirstNum(10);
-		myCalucator.setSecondNum(2);
+		myCalucator.setFirstNum(30);
+		myCalucator.setSecondNum(5);
 		
 		myCalucator.add();
 		myCalucator.sub();
