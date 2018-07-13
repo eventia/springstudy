7d07516017feb5a733b38d1bef85a74b764a45c2
lsk <eventia@paran.com>
2018-07-13 11:59:12 +0900

    SpringLecture 2018-07-13

--- s10a-aspect-annot/src/main/java/com/wind/s10/LogAop.java
+++ s10a-aspect-annot/src/main/java/com/wind/s10/LogAop.java
@@ -8,7 +8,20 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LogAop {
+
 	
+// 	@Pointcut("execution(public void get*(..))")	// public void인 모든 get메소드
+// 	@Pointcut("execution(* com.wind.s10.*.*())") 	// com.wind.s10 패키지에 파라미터가 없는 모든 메소드
+// 	@Pointcut("execution(* com.wind.s10..*.*())")	// com.wind.s10 패키지 & com.wind.sp10 하위 패키지에 파라미터가 없는 모든 메소드
+// 	@Pointcut("execution(* com.wind.s10.Worker.*())")	// com.wind.s10.Worker 안의 모든 메소드
+
+// 	@Pointcut("within(com.wind.s10.*)")	//com.wind.s10 패키지 안에 있는 모든 메소드
+// 	@Pointcut("within(com.wind.s10..*)")  //com.wind.s10 패키지 및 하위 패키지 안에 있는 모든 메소드
+//	@Pointcut("within(com.wind.s10.Worker)") //com.wind.s10.Worker 모든 메소드
+		
+// 	@Pointcut("bean(student)")	//student 빈에만 적용
+// 	@Pointcut("bean(*ker)")		//~ker로 끝나는 빈에만 적용
+
 	@Pointcut("within(com.wind.s10.*)")
 	private void pointcutMethod() {
 	}
