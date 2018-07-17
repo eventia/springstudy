428ce3ef38b942fd67d173224bf42ed19ab0cbda
lsk <eventia@paran.com>
2018-07-17 11:01:58 +0900

    SpringLecture 2018-07-17

--- sw13a-request/src/main/java/com/wind/web/comtroller/HttpRequestController.java
+++ sw13a-request/src/main/java/com/wind/web/comtroller/HttpRequestController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -65,5 +66,17 @@ public class HttpRequestController {
 	public String joinDataSimple(Member member) {
 		return "member/join";
 	}
+	
+	@RequestMapping("member/{memberId}")
+	public String getMemberId(
+			@PathVariable String memberId,
+			Model model
+			) {
+		
+		model.addAttribute("memberId", memberId);
+		return "member/memberView";
+	}
+	
+	
 	
 }
