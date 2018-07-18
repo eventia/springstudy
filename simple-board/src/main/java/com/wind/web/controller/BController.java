d65a7f778e09f43b1506644b6a6bc4088f30ce6b
lsk <eventia@paran.com>
2018-07-18 14:50:39 +0900

    SpringLecture 2018-07-18

--- simple-board/src/main/java/com/wind/web/controller/BController.java
+++ simple-board/src/main/java/com/wind/web/controller/BController.java
@@ -1,5 +1,23 @@
 package com.wind.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wind.web.service.BListService;
+import com.wind.web.service.BService;
+
+@Controller
 public class BController {
+	
+	BService service = null;
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		service  = new BListService();
+		service.execute(model);
+		return "list";
+	}
+	
 
 }
